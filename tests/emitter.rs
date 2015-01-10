e6e3cd6d73e600b3ea926225a6014aef97ae8772
kentaromiura <kentaromiura@gmail.com>
2015-01-10 17:08:27 +0000

    upgrade to rust 1.0.0 alpha nightly

--- tests/emitter.rs
+++ tests/emitter.rs
@@ -6,23 +6,27 @@ mod event;
 
 #[test]
 fn must_register_an_event_and_call_callback_passing_data() {
-    let mut emitter = event::Emitter::new();
-    let test = String::from_str("TEST");
-    let data_inside = String::from_str("DATA INSIDE");
-
-    emitter.on(test.clone(), |event:& mut HashMap<String, String>|{
-
-        match event.find(&test) {
-          Some(thing) => {
-            if *thing != data_inside {
-              fail!("the data inside the event is not what is expected, expected {}, get {}", data_inside, *thing);
+    let (test, data_inside) = (
+        "TEST".to_string(),
+        "DATA INSIDE".to_string()
+    );
+    let (mut emitter, callback) = (
+        event::Emitter::new(),       
+        & mut |event:& mut HashMap<String, String>| {
+            match event.get(&test) {
+              Some(thing) => {
+                if *thing != data_inside {
+                  panic!("the data inside the event is not what is expected, expected {}, get {}", data_inside, *thing);
+                }
+              },
+              None => {
+                panic!("the data expected wasn't found")
+              },
             }
-          },
-          None => {
-            fail!("the data expected wasn't found")
-          },
         }
-    });
+    );
+    
+    emitter.on(test.clone(), callback);
 
     let mut data : HashMap<String, String> = HashMap::new();
     data.insert(test.clone(), data_inside.clone());
@@ -31,36 +35,39 @@ fn must_register_an_event_and_call_callback_passing_data() {
 
 #[test]
 fn must_register_multiple_events_and_call_all_the_callback_passing_a_copy_of_the_same_data() {
-    let mut emitter = event::Emitter::new();
-    let test = String::from_str("TEST");
-    let data_inside = String::from_str("DATA INSIDE");
-
-    emitter.on(test.clone(), |event:& mut HashMap<String, String>|{
-
-        match event.find(&test) {
+    let (test, data_inside) = (
+        "TEST".to_string(),
+        "DATA INSIDE".to_string()
+    );
+    let (mut emitter, callback, second_callback) = (event::Emitter::new(),
+    & mut |event:& mut HashMap<String, String>|{
+        match event.get(&test) {
           Some(thing) => {
             if *thing != data_inside {
-              fail!("the data inside the event is not what is expected, expected {}, get {}", data_inside, *thing);
+              panic!("the data inside the event is not what is expected, expected {}, get {}", data_inside, *thing);
             }
           },
           None => {
-            fail!("the data expected wasn't found")
+            panic!("the data expected wasn't found")
+          },
+        }
+    },
+    & mut |event:& mut HashMap<String, String>|{
+        match event.get(&test) {
+          Some(thing) => {
+            if *thing != data_inside {
+              panic!("the data inside the event is not what is expected, expected {}, get {}", data_inside, *thing);
+            }
+          },
+          None => {
+            panic!("the data expected wasn't found")
           },
         }
     });
 
-    emitter.on(test.clone(), |event:& mut HashMap<String, String>|{
-        match event.find(&test) {
-          Some(thing) => {
-            if *thing != data_inside {
-              fail!("the data inside the event is not what is expected, expected {}, get {}", data_inside, *thing);
-            }
-          },
-          None => {
-            fail!("the data expected wasn't found")
-          },
-        }
-    });
+    emitter.on(test.clone(), callback);
+    
+    emitter.on(test.clone(), second_callback);
 
     let mut data : HashMap<String, String> = HashMap::new();
     data.insert(test.clone(), data_inside.clone());
@@ -69,13 +76,14 @@ fn must_register_multiple_events_and_call_all_the_callback_passing_a_copy_of_the
 
 #[test]
 fn must_unregister_an_event() {
-    let mut emitter = event::Emitter::new();
-    let test = String::from_str("TEST");
-    let data_inside = String::from_str("DATA INSIDE");
-
-    emitter.on(test.clone(), |event: & mut HashMap<String, String>|{
-        fail!("the emitter must never be called instead has been called with this parameters {}", event);
+    let (test, data_inside) = (
+        "TEST".to_string(),
+        "DATA INSIDE".to_string()
+    );
+    let (mut emitter, callback) = (event::Emitter::new(), & mut |event: & mut HashMap<String, String>|{
+        panic!("the emitter must never be called instead has been called with {} parameters", event.capacity());
     });
+    emitter.on(test.clone(), callback);
 
     let mut data : HashMap<String, String> = HashMap::new();
     data.insert(test.clone(), data_inside.clone());
@@ -89,27 +97,35 @@ fn must_work_with_a_custom_event_data_and_allow_the_callback_to_modified_it(){
     struct Foo {
       bar : String
     };
-
-    let mut emitter = event::Emitter::new();
-    let test = String::from_str("TEST");
-    let data_inside = String::from_str("DATA INSIDE");
-
-    emitter.on(test.clone(), |event:& mut HashMap<String, Foo>|{
-        match event.find(&test) {
+    let (test, data_inside) = (
+        "TEST".to_string(),
+        "DATA INSIDE".to_string()
+    );
+    let (mut emitter, callback) = (event::Emitter::new(), & mut |event:& mut HashMap<String, Foo>|{
+        match event.get(&test) {
           Some(thing) => {
             if (*thing).bar != data_inside {
-              fail!("the data inside the event is not what is expected, expected {}, get {}", data_inside, (*thing).bar);
+              panic!("the data inside the event is not what is expected, expected {}, get {}", data_inside, (*thing).bar);
             }
           },
           None => {
-            fail!("the data expected wasn't found")
+            panic!("the data expected wasn't found")
           },
         }
-        event.insert(String::from_str("Result"), Foo {bar: String::from_str("HAHAHA")});
+        event.insert("Result".to_string(), Foo {bar: "HAHAHA".to_string()});
     });
+    
+    emitter.on(test.clone(), callback);
 
     let mut data : HashMap<String, Foo> = HashMap::new();
     data.insert(test.clone(), Foo {bar: data_inside.clone()});
     emitter.emit(test.clone(), & mut data);
-    if data[String::from_str("Result")].bar != String::from_str("HAHAHA") { fail!("Doesn't pass back values") }
+    match data.get(&"Result".to_string()) {
+        Some(ref data) => {
+            if data.bar != "HAHAHA".to_string() {
+                panic!("Doesn't pass back values")
+            }
+        }
+        _ => { panic!("Doesn't pass back values") }
+    }
 }
