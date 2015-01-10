9436cd7cbe7f9631abbc02488a64ea3e4ec7cad6
kentaromiura <kentaromiura@gmail.com>
2015-01-10 17:08:27 +0000

    upgrade to rust 1.0.0 alpha nightly

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 use std::collections::HashMap;
 
 pub type EventData<T> = HashMap<String, T>;
-pub type EventCallback<'callback, T> = |& mut EventData<T>|:'callback;
-
+//pub type EventCallback<'callback, T> = |& mut EventData<T>|:'callback;
+pub type EventCallback<'callback, T> = FnMut(& mut EventData<T>) -> () + 'callback;
 
 pub trait Events<'callback, T> {
-  fn on(&mut self, event_name: String, callback: EventCallback<'callback, T>);
+  fn on(&mut self, event_name: String, callback: &'callback mut EventCallback<'callback, T>);
   fn off(&mut self, event_name: String);
   fn emit(&mut self, event_name: String, EventData: & mut EventData<T>);
 }
 
 pub struct Emitter<'callback, T> {
-  events : HashMap<String, Vec<EventCallback<'callback, T>>>
+  events : HashMap<String, Vec<&'callback mut EventCallback<'callback, T>>>
 }
 
 impl<'callback, T> Emitter<'callback, T>{
@@ -23,9 +23,12 @@ impl<'callback, T> Emitter<'callback, T>{
 }
 
 impl<'callback, T> Events<'callback, T> for Emitter<'callback, T> {
-  fn on(&mut self, event_name: String, callback: EventCallback<'callback, T>) {
+  fn on(&mut self, event_name: String, callback: &'callback mut EventCallback<'callback, T>) {
     if self.events.contains_key(&event_name) {
-      self.events.get_mut(&event_name).push(callback);
+      match self.events.get_mut(&event_name) {
+          Some(callbacks) => callbacks.push(callback),
+          None => (),
+      }
     } else {
       let vec = vec![callback];
       self.events.insert(event_name, vec);
@@ -36,7 +39,7 @@ impl<'callback, T> Events<'callback, T> for Emitter<'callback, T> {
     self.events.remove(&event_name);
   }
   fn emit(&mut self, event_name: String, event_data: & mut EventData<T>) {
-    match self.events.find_mut(&event_name) {
+    match self.events.get_mut(&event_name) {
       Some(callbacks) => {
         for callback in callbacks.iter_mut() {
             (*callback)(event_data);
